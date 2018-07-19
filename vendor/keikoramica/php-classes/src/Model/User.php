d601667b19d08a48c618cf50848b38564d417bcb
Estefani Fujimoto <estefani.fujimoto@hotmail.com>
2018-07-19 17:42:14 -0300

    Configurando login

---
+++ vendor/keikoramica/php-classes/src/Model/User.php
@@ -0,0 +1,140 @@
+<?php 
+
+namespace ekfuji\Model;
+
+use \ekfuji\Model;
+use \ekfuji\DB\Sql;
+
+class User extends Model {
+
+	const SESSION = "User";
+
+	protected $fields = [
+		"iduser", "idperson", "deslogin", "despassword", "inadmin", "dtergister"
+	];
+
+	public static function login($login, $password):User
+	{
+
+		$db = new Sql();
+
+		$results = $db->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
+			":LOGIN"=>$login
+		));
+
+		if (count($results) === 0) {
+			throw new \Exception("Usuário inexistente ou senha inválida.");
+		}
+
+		$data = $results[0];
+
+		if (password_verify($password, $data["despassword"])) {
+
+			$user = new User();
+			$user->setData($data);
+
+			$_SESSION[User::SESSION] = $user->getValues();
+
+			return $user;
+
+		} else {
+
+			throw new \Exception("Usuário inexistente ou senha inválida.");
+
+		}
+
+	}
+
+	public static function logout()
+	{
+
+		$_SESSION[User::SESSION] = NULL;
+
+	}
+
+	public static function verifyLogin($inadmin = true)
+	{
+
+		if (
+			!isset($_SESSION[User::SESSION])
+			|| 
+			!$_SESSION[User::SESSION]
+			||
+			!(int)$_SESSION[User::SESSION]["iduser"] > 0
+			||
+			(bool)$_SESSION[User::SESSION]["iduser"] !== $inadmin
+		) {
+			
+			header("Location: /admin/login");
+			exit;
+
+		}
+
+	}
+	public static function listAll()
+	{
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+	}
+
+	public function save()
+	{
+		$sql = new Sql();
+		/*
+			pdesperson VARCHAR(64), 
+			pdeslogin VARCHAR(64), 
+			pdespassword VARCHAR(256), 
+			pdesemail VARCHAR(128), 
+			pnrphone BIGINT, 
+			pinadmin TINYINT
+		*/
+
+		$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", 
+			array(
+				":desperson"=>$this->getdesperson(), 
+				":deslogin"=>$this->getdeslogin(), 
+				":despassword"=>$this->getdespassword(), 
+				":desemail"=>$this->getdesemail(), 
+				":nrphone"=>$this->getnrphone(), 
+				":inadmin"=>$this->getinadmin() 
+			));
+		$this->setData($results[0]);
+	}
+
+	public function get($iduser)
+	{
+		$sql = new Sql();
+		$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser", array(":iduser"=>$iduser));
+
+			$this->setData($results[0]);
+	
+	}
+
+	public function update()
+	{
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", 
+			array(
+				":iduser"=>$this->getiduser(), 
+				":desperson"=>$this->getdesperson(), 
+				":deslogin"=>$this->getdeslogin(), 
+				":despassword"=>$this->getdespassword(), 
+				":desemail"=>$this->getdesemail(), 
+				":nrphone"=>$this->getnrphone(), 
+				":inadmin"=>$this->getinadmin() 
+			));
+		$this->setData($results[0]);
+	}
+
+	public function delete()
+	{
+		$sql = new Sql();
+
+		$sql->query("CALL sp_users_delete(:iduser)", array(":iduser"=>$this->getiduser()));
+	}
+
+}
+
+
+?>
